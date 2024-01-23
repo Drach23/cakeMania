8e404c6761f3c9528a64aeb7b4636495ab846e85
Drach <88101742+Drach23@users.noreply.github.com>
2024-01-22 22:38:25 -0600

    Agregado de base para el backend

---
+++ cakeMania/src/main/java/com/unitech/cakemania/CakeManiaApplication.java
@@ -0,0 +1,13 @@
+package com.unitech.cakemania;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CakeManiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CakeManiaApplication.class, args);
+	}
+
+}
---
+++ cakeMania/src/main/java/com/unitech/cakemania/Models/UserModel.java
@@ -0,0 +1,66 @@
+package com.unitech.cakemania.Models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class UserModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false,unique = true)
+    private Long id;
+    private String name;
+    private String lastName;
+    private String email;
+    private String password;
+    private String phone;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
+
