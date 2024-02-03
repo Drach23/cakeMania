9f6fbe26ce7eadae177eb0be33773da984a9b0a9
Drach <88101742+Drach23@users.noreply.github.com>
2024-02-02 21:51:13 -0600

    Agregado del request para register

--- cakeMania/src/main/java/com/unitech/cakemania/Auth/RegisterRequest.java
+++ cakeMania/src/main/java/com/unitech/cakemania/Auth/RegisterRequest.java
@@ -1,2 +1,19 @@
-package com.unitech.cakemania.Auth;public class RegisterRequest {
+package com.unitech.cakemania.Auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+    private String username;
+    private String name;
+    private String lastName;
+    private String email;
+    private String password;
+    private String phone;
 }
