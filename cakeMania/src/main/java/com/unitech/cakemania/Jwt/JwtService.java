510e0c59a2b172f7cc2b3c63a8d29a5e61fe6755
Drach <88101742+Drach23@users.noreply.github.com>
2024-02-02 21:59:17 -0600

    Agregado de archivo base

--- cakeMania/src/main/java/com/unitech/cakemania/Jwt/JwtService.java
+++ cakeMania/src/main/java/com/unitech/cakemania/Jwt/JwtService.java
@@ -1,12 +1,11 @@
-package com.unitech.cakemania.Auth;
+package com.unitech.cakemania.Jwt;
 
-import com.unitech.cakemania.User.UserRole;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtService {
     public String getToken(UserDetails userDetails) {
-        return null
+        return null;
     }
 }
