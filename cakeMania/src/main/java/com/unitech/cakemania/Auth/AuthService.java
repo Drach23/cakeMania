66a1f0d3c9eecfe02fa5d5703111ac60093f7691
Drach <88101742+Drach23@users.noreply.github.com>
2024-02-02 21:50:07 -0600

    Implementar AuthService para gestionar autenticación y registro
    - Se añade un servicio AuthService con métodos para manejar autenticación y registro.
    - Se utiliza UserRepository para acceder a la base de datos de usuarios.
    - Se integra JwtService para la generación de tokens JWT.
    - Se emplea PasswordEncoder para cifrar contraseñas.
    - Se implementan métodos 'login' y 'register'.
    - En el método 'register', se crea un nuevo usuario en la base de datos con los datos proporcionados en la solicitud y se devuelve un token JWT como respuesta.

--- cakeMania/src/main/java/com/unitech/cakemania/Auth/AuthService.java
+++ cakeMania/src/main/java/com/unitech/cakemania/Auth/AuthService.java
@@ -1,2 +1,38 @@
-package com.unitech.cakemania.Auth;public class AuthService {
+package com.unitech.cakemania.Auth;
+
+import com.unitech.cakemania.Jwt.JwtService;
+import com.unitech.cakemania.User.Role;
+import com.unitech.cakemania.Models.UserModel;
+import com.unitech.cakemania.Repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final UserRepository userRepository;
+    private final JwtService jwtService;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthResponse login(LoginRequest request) {
+        return null;
+    }
+
+    public AuthResponse register(RegisterRequest request) {
+        UserModel userModel = UserModel.builder()
+                .username(request.getUsername())
+                .username(request.getName())
+                .username(request.getLastName())
+                .username(request.getEmail())
+                .username(request.getPassword())
+                .username(request.getPhone())
+                .role(Role.USER)
+                .build();
+        userRepository.save(userModel);
+
+        return AuthResponse.builder()
+                .token(jwtService.getToken(userModel))
+                .build();
+    }
 }
