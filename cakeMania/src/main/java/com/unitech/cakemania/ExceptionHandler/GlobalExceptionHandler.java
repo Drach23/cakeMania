192c12c2e143d985eef69146b43a22abf9dcc215
Drach <88101742+Drach23@users.noreply.github.com>
2024-02-04 23:21:01 -0600

    Manejador de errores

--- cakeMania/src/main/java/com/unitech/cakemania/ExceptionHandler/GlobalExceptionHandler.java
+++ cakeMania/src/main/java/com/unitech/cakemania/ExceptionHandler/GlobalExceptionHandler.java
@@ -1,2 +1,22 @@
-package com.unitech.cakemania.ExceptionHandler;public class GlobalExceptionHandler {
+package com.unitech.cakemania.ExceptionHandler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handlerArgumentException(IllegalArgumentException ex)
+    {
+        return new ResponseEntity<String>(ex.getMessage(),HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handlerRuntimeException(RuntimeException ex)
+    {
+        return new ResponseEntity<String>(ex.getMessage(),HttpStatus.BAD_GATEWAY);
+    }
 }
